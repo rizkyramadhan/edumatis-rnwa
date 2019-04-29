72a90076bea75707a0bf124254cc20f5a65a9ee6
rizky <rizky@andromedia.co.id>
2019-04-29 20:26:38 +0700

    fix

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,28 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- *
- * @format
- */
-
 import React from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+
+import Router from "./components/nav/Router";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
+  android: "Double tap R on your keyboard to reload,\n Shake or press menu enu"
 });
 export default () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+      <Text style={styles.welcome}>
+        geje mantb asd asd ini bisa React Native!
+      </Text>
+      <Text style={styles.instructions}>To get asdstarted, edit App.tsx</Text>
       <Text style={styles.instructions}>{instructions}</Text>
+      <TouchableOpacity style={styles.instructions}>
+        <Router />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -31,7 +32,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#F5FCFF"
   },
   welcome: {
